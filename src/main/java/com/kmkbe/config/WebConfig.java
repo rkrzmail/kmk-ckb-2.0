617d1055744a2f6733f9515b7e831b1ac9e3bdf6
Achmad Ari Wibowo <achmadarw@gmail.com>
2026-08-11 14:09:42 +0700

    Add missing import for Value annotation in WebConfig

--- src/main/java/com/kmkbe/config/WebConfig.java
+++ src/main/java/com/kmkbe/config/WebConfig.java
@@ -2,6 +2,7 @@ package com.kmkbe.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.ByteArrayHttpMessageConverter;
@@ -13,6 +14,7 @@ import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
 import org.springframework.web.servlet.config.annotation.*;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.Executors;
 
