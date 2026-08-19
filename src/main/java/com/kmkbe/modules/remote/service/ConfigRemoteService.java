7d7816c449eb2a3f3bb1d7922cf1cecba1abf2aa
hyvercode <hyvercode@gmail.com>
2026-08-19 16:06:16 +0700

    change path email danasakti-tmt

--- src/main/java/com/kmkbe/modules/remote/service/ConfigRemoteService.java
+++ src/main/java/com/kmkbe/modules/remote/service/ConfigRemoteService.java
@@ -36,7 +36,7 @@ public class ConfigRemoteService {
         try {
             final BaseLdapRemoteResponseDto<String> tokenResponse = authRemoteService.fetchAuthJwt();
 
-            final String url = siscaUrlWhiteList + "/authconfig/mail/danasakti-mail";
+            final String url = siscaUrlWhiteList + "/authconfig/mail/danasakti-tmt";
             final HttpHeaders headers = new HttpHeaders();
             headers.setBearerAuth(tokenResponse.getData());
 
