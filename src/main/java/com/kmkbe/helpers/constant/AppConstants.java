8f6b7dfc1d73d3af7775f0add12dad50a3dbd96e
hyvercode <hyvercode@gmail.com>
2026-09-22 14:24:24 +0700

    remove customer

--- src/main/java/com/kmkbe/helpers/constant/AppConstants.java
+++ src/main/java/com/kmkbe/helpers/constant/AppConstants.java
@@ -9,7 +9,6 @@ public class AppConstants {
   public static final String LANG_EN = "en-ID";
   public static final String CREATOR_CONFINS = "confins";
   public static final String NEW_CUSTOMER = "new";
-  public static final String EXISTING_CUSTOMER = "exist";
 
   public static final Integer CODE_OK = 200;
   public static final Integer CODE_FAILED = 500;
